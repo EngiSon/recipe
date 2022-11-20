5fcad826eb22c7aa2df0b5e0e8b29679ed2f872d
EngiSon <lleopardchaun@gmail.com>
2022-11-20 12:58:48 +0100

    Implemented models for Recipe and User

---
+++ RecipeAPI/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿namespace RecipeAPI.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public FoodType[]? FoodType { get; set; }
+        public string[]? Ingredients { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public enum FoodType
+    {
+        Vegan,
+        Vegetarian,
+        Pescitarian,
+        Glutenfree
+    }
+}
