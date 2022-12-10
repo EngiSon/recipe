132aca56abaac75b3cf355cf8273be037f268a84
EngiSon <lleopardchaun@gmail.com>
2022-12-10 12:46:26 +0100

    Migrations added, recipe and user controllers implemented

---
+++ RecipeAPI/Controllers/RecipeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Dal;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/Recipes")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly RecipeContext ctx;
+
+        public RecipeController(RecipeContext context)
+        {
+            this.ctx = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipes()
+        {
+            return await ctx.Recipes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(int id)
+        {
+            var recipe = await ctx.Recipes.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return recipe;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(RecipeDTO recipe)
+        {
+            var author = ctx.Users.Find(recipe.UserId);
+
+            if (author == null ||
+                !checkEnum(recipe.FoodType)
+                )
+            {
+                return BadRequest();
+            }
+
+            var newRecipe = DTOtoItem(recipe, author);
+
+            ctx.Recipes.Add(newRecipe);
+            await ctx.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRecipe), new { id = recipe.Id }, recipe);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(int id)
+        {
+            var recipe = await ctx.Recipes.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            ctx.Recipes.Remove(recipe);
+            await ctx.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private static Recipe DTOtoItem(RecipeDTO dto, User author)
+        {
+            return new Recipe(author, dto.Name, dto.FoodType, dto.Ingredients, dto.Description);
+        }
+
+        private static Boolean checkEnum(String foodType)
+        {
+            try
+            {
+                Enum.Parse(typeof(FoodType), foodType, true);
+            } catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ RecipeAPI/Controllers/UserController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeAPI.Dal;
+using RecipeAPI.Models;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/Users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly RecipeContext ctx;
+
+        public UserController(RecipeContext context)
+        {
+            ctx = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await ctx.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await ctx.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> ValidateUser(ValidationDTO dto)
+        {
+            var foundUser = await ctx.Users.Where(u => (u.Username == dto.Username) && (u.Password == dto.Password)).FirstAsync();
+
+            if (foundUser == null)
+            {
+                return BadRequest();
+            }
+
+            return foundUser;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(UserDTO user)
+        {
+            if (ctx.Users.Where(u => u.Username == user.Username).Any()) 
+            { 
+                return BadRequest();
+            }
+
+            var newUser = DTOtoItem(user);
+
+            ctx.Users.Add(newUser);
+            await ctx.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new {id = user.Id}, user);
+        }
+
+        [HttpPut("{favoritingUserId}/{favoritedUserId}")]
+        public async Task<ActionResult> HandleFavorite(int favoritingUserId, int favoritedUserId)
+        {
+            var favoritingUser = await ctx.Users.FindAsync(favoritingUserId);
+            var favoritedUser = await ctx.Users.FindAsync(favoritedUserId);
+
+            if (favoritingUser == null ||
+                favoritedUser == null)
+            {
+                return BadRequest();
+            }
+
+            favoritingUser.OutgoingFavorites.Add(favoritedUser);
+            favoritedUser.IncomingFavorites.Add(favoritingUser);
+
+           await ctx.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private static User DTOtoItem(UserDTO dto)
+        {
+            return new User(dto.Username, dto.Password, dto.Email);
+        }
+    }
+}
